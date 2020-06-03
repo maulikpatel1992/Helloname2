eb1da433d0aaa0bd44e9c3970c381c367cfe4b94
Maulik <mvp46@njit.edu>
2020-06-02 22:15:54 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Helloname2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string fname = "Mike";
+            string lname = "Matt";
+
+            Console.WriteLine("My name is {0} {1}", fname, lname);
+
+        }
+    }
+}
